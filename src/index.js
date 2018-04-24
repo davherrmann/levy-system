7c65380ac8e7eb313760745903dd328b2d7a2940
David Herrmann <davherrmann@googlemail.com>
2018-04-24 13:04:22 +0200

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
+import * as firebase from "firebase"
+
+var config = {
+  apiKey: "AIzaSyCqN3e3Xmi_sc7thEa3eIvDLnm0Ov9vlDA",
+  authDomain: "levy-system.firebaseapp.com",
+  databaseURL: "https://levy-system.firebaseio.com",
+  projectId: "levy-system",
+  storageBucket: "levy-system.appspot.com",
+  messagingSenderId: "793947371968",
+}
+firebase.initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
