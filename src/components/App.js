5d4e86296387a025f078d401c957c49832e9473f
David Herrmann <davherrmann@googlemail.com>
2018-04-24 15:15:07 +0200

    add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Frame from "./Frame"
 import Login from "./Login"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Login />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Frame} />
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Frame.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default () => (
+  <header>
+    <nav>
+      <Link to="Logout">Logout</Link>
+    </nav>
+  </header>
+)
